ad4449f38a16bc253d7855c6cbddb901fbd35e95
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2023-08-07 11:47:11 +0530

    Created User Login API

--- server/src/controllers/authentication.ts
+++ server/src/controllers/authentication.ts
@@ -2,8 +2,38 @@ import {  createUser, getUserByEmail } from '../db/user';
 import express from 'express';
 import { authentication, random } from '../helpers/index';
 
+export const login = async (req:express.Request , res: express.Response) =>{
+    try {
+        const {email , password} = req.body;
+        if(!email || !password ){
+            return res.status(400).send("Email or password field is not provided");
+            
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if(!user){
+            return res.status(400).send("User is not registered");
+        }
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password != expectedHash) {
+            return res.sendStatus(403).send("Password is incorrect");
+          }
+      
+          const salt = random();
+          user.authentication.sessionToken = authentication(salt, user._id.toString());
+      
+          await user.save();
+      
+          res.cookie('Google-meet-auth', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+          console.log("User Logged in")
+          return res.status(200).json(user).end();
+    } catch (error) {
+        console.error(error);
+        res.status(400).send("Some error happended in login api")
+    }
+}
+
 export const register =async (req:express.Request,res:express.Response) => {
-    console.log("In the register function")
+    // console.log("In the register function")
     try {
         const {username,email ,password} = req.body;
         console.log({username,email,password})
